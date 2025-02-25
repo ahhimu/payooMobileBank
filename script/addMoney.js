57b3fc825f314928fb53885f64e539e3c51f96b2
ahhimu <asasanhabibhimu.bc@gmail.com>
2025-02-25 23:28:53 +0600

    Add Money Update

---
+++ script/addMoney.js
@@ -0,0 +1,30 @@
+document.getElementById('add-money-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    const amount = document.getElementById('amount').value;
+    const convertedAmount = parseFloat(amount);
+  
+    const pin = document.getElementById('pin').value;
+    const convertedPin = parseInt(pin);
+
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const convertedMainBalance = parseFloat(mainBalance);
+
+   
+
+    if(convertedAmount >= 50){
+  
+        if(convertedPin === 1234){
+            // ghotona gotamu eikhanei
+            const sum = convertedMainBalance + convertedAmount;
+          
+            document.getElementById('main-balance').innerText = sum;
+        }
+        else{
+            alert("Provide Valid Pin !");
+        }
+    }
+    else{
+        alert("Amount Minimum 50 ");
+    }
+
+})
